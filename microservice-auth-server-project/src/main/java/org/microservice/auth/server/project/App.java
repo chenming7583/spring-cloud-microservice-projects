8a35ac29525db5884e565d607bb8d12680631e09
chenm <20chenming08@163.com>
2017-07-05 01:00:11 +0800

    初始化登录认证模块

---
+++ microservice-auth-server-project/src/main/java/org/microservice/auth/server/project/App.java
@@ -0,0 +1,13 @@
+package org.microservice.auth.server.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
