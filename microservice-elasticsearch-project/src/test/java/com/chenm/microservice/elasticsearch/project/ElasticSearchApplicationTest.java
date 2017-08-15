83a2dd3633fb19ca6063c45cac83a3ca7a3ed35e
chenm <20chenming08@163.com>
2017-08-16 02:11:48 +0800

    代码更新

--- microservice-elasticsearch-project/src/test/java/com/chenm/microservice/elasticsearch/project/ElasticSearchApplicationTest.java
+++ microservice-elasticsearch-project/src/test/java/com/chenm/microservice/elasticsearch/project/ElasticSearchApplicationTest.java
@@ -2,11 +2,18 @@ package com.chenm.microservice.elasticsearch.project;
 
 import java.io.IOException;
 
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsRequest;
 import org.elasticsearch.action.admin.indices.exists.indices.IndicesExistsResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.ClusterAdminClient;
+import org.elasticsearch.client.Requests;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.health.ClusterHealthStatus;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.junit.Ignore;
@@ -20,6 +27,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * elasticsearch api 测试用例
  * @author chenm
  *
+ * 文档介绍
+ * http://blog.csdn.net/eff666/article/details/52432181
+ * http://www.cnblogs.com/luxiaoxun/archive/2015/10/11/4869509.html
  */
 @RunWith(SpringJUnit4ClassRunner.class)  //SpringJUnit支持，由此引入Spring-Test框架支持
 @SpringBootTest(classes=ElasticSearchApplication.class) //指定SpringBoot工程的Application启动类 
@@ -34,10 +44,27 @@ public class ElasticSearchApplicationTest
 	private String typeName = "typeTest";
 	
 	/**
-	 * 创建索引
+	 * 集群状态
 	 */
 	@Test
 	@Ignore
+    public void clusterStatus(){
+        //注意集群的client获取方式略有不同，
+        ClusterAdminClient clusterAdminClient = client.admin().cluster();
+        ClusterHealthResponse healths = clusterAdminClient.prepareHealth().get();
+        String clusterName = healths.getClusterName();
+        int numberOfDataNodes = healths.getNumberOfDataNodes();
+        int numberOfNodes = healths.getNumberOfNodes();
+        ClusterHealthStatus status = healths.getStatus();
+        System.out.println(clusterName+"###"+numberOfDataNodes+"###"+status.name());
+
+    }
+	
+	/**
+	 * 创建索引
+	 */
+	@Test
+//	@Ignore
 	public void createIndex(){
 		client.admin().indices().create(new CreateIndexRequest(indexName)).actionGet();
 	}
@@ -60,25 +87,67 @@ public class ElasticSearchApplicationTest
 	@Test
 	@Ignore
 	public void deleteType(){
-		client.prepareDelete().setIndex(indexName).setType("type").execute().actionGet();
+		client.prepareDelete().setIndex(indexName).setType(typeName).execute().actionGet();
 	}
 	
 	/**
 	 * 定义索引的映射类型(mapping)
 	 * http://blog.csdn.net/eff666/article/details/52432181
+	 * http://www.cnblogs.com/luxiaoxun/archive/2015/10/11/4869509.html
 	 */
 	@Test
+	@Ignore
 	public void defineIndexTypeMapping(){
 		try {
-			XContentBuilder mapBuilder = XContentFactory.jsonBuilder();
+			XContentBuilder builder = XContentFactory.jsonBuilder()
+					.startObject()
+	                .startObject("blog")
+	                .startObject("properties")
+	                .startObject("id").field("type", "integer").field("store", "yes").endObject()
+	                .startObject("title").field("type", "string").field("store", "yes").endObject()
+	                .startObject("content").field("type", "string").field("store", "yes").endObject()
+	                .endObject()
+	                .endObject()
+	                .endObject();
+			 PutMappingRequest mappingRequest = Requests.putMappingRequest("test").type("blog").source(builder);
+		        client.admin().indices().putMapping(mappingRequest).actionGet();
+
+					
 		} catch (IOException e) {
 			e.printStackTrace();
 		} 
 	}
 	
+	 /**
+     * 创建数据
+     * 
+     */
 	@Test
 	@Ignore
-	public void add(){
-		
-	}
+    public void createData() {
+        try {
+			// 创建数据json 注意此ID是一个字段不是上面查询的id
+			   XContentBuilder builder = XContentFactory.jsonBuilder()
+			           .startObject()
+			           .field("id", "2")
+			           .field("title", "我是标题")
+			           .field("content", "我是内容")
+			           .endObject();
+			IndexResponse indexResponse = client.prepareIndex("test","blog").setSource(builder).execute().actionGet();
+			System.out.println(indexResponse.status());
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+	
+	 /**
+     * 通过ID查询数据，
+     * test为索引库，blog为类型，id为标识
+     */
+	@Test
+	@Ignore
+    public GetResponse get(){
+        return  client.prepareGet("test", "blog", "2").get();
+    }
+
 }
