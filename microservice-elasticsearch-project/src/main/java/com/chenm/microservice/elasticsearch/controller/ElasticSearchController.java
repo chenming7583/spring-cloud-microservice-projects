b3d237a4621d42fe870a4c5d7c5c70cea5d76832
chenm <20chenming08@163.com>
2017-09-26 01:53:39 +0800

    搜索静态页面配置

--- microservice-elasticsearch-project/src/main/java/com/chenm/microservice/elasticsearch/controller/ElasticSearchController.java
+++ microservice-elasticsearch-project/src/main/java/com/chenm/microservice/elasticsearch/controller/ElasticSearchController.java
@@ -1,10 +1,14 @@
 package com.chenm.microservice.elasticsearch.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.elasticsearch.client.transport.TransportClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Controller
  * @author chenm
@@ -17,12 +21,22 @@ public class ElasticSearchController {
 //	@Autowired
 //	private TransportClient client;
 	
-	@RequestMapping("/index/{id}")
-	public String index(@PathVariable String id){
+	@RequestMapping("/index")
+	public String index(){
 		
 //		GetResponse response = client.prepareGet("megacorp", "employee", id).get();
 //		System.out.println(response);
 		
 		return "index";
+	}
+	
+	@RequestMapping("/ajax/sug")
+	@ResponseBody
+	public List<String> sug(){
+	    List<String> result = new ArrayList<String>();
+	    result.add("aa");
+	    result.add("bb");
+	    result.add("cc");
+		return result;
 	}
 }
